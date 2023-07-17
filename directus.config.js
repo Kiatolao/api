bb311ab8b494d052c23b55dca55768bccd325865
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-07-17 04:41:50 +0200

    setting up aws

--- directus.config.js
+++ directus.config.js
@@ -21,5 +21,15 @@ module.exports = function (env) {
         DB_DATABASE: env.PGDATABASE,
         DB_USER: env.PGUSER,
         DB_PASSWORD: env.PGPASSWORD,
+
+        STORAGE_LOCATIONS:"local,aws",
+
+        STORAGE_LOCAL_DRIVER:"local",
+        STORAGE_LOCAL_ROOT:"local",
+
+        STORAGE_AWS_KEY:"AKIAWJE2OQ3YDIV4VXN4",
+        STORAGE_AWS_SECRET:"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",
+        STORAGE_AWS_REGION:"eu-west-3",
+        STORAGE_AWS_BUCKET:"charles-cantin",
     };
 };
